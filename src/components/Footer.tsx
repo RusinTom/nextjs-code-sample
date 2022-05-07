568dafad7a1e16c31a9b96deda930c571deca171
RusinTomasz <tomaszrusin90@gmail.com>
2022-05-07 11:26:43 +0200

    feat: add logo and styles for Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import LogoGray from '../assets/logo-gray.svg';
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-black px-10 pt-[97px] pb-[85px]'>
+      <LogoGray className='m-auto h-[23px] w-[195px]' />
+    </footer>
+  );
 };
