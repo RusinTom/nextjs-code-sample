ed2c13f829573b11b83eebbe30b250b31028f77c
RusinTomasz <tomaszrusin90@gmail.com>
2022-05-09 19:00:28 +0200

    feat: add fakeData file

---
+++ src/data/fakeData.ts
@@ -0,0 +1,42 @@
+import { NavigationItem } from '@/types/NavigationItem';
+
+export const navigation: NavigationItem[] = [
+  { key: '#navItem1', label: 'Lorem', url: '/' },
+  { key: '#navItem2', label: 'Lorem', url: '/' },
+];
+
+export const fakeBoxes = [
+  {
+    id: '#box1',
+    title: 'Lorem ipsum',
+    category: 'Accessories & Ears',
+    percentage: 9,
+  },
+  { id: '#box2', title: 'Lorem ipsum', category: 'Arms', percentage: 15 },
+  {
+    id: '#box3',
+    title: 'Lorem ipsum',
+    category: 'Background',
+    percentage: 2,
+  },
+  { id: '#box4', title: 'Lorem ipsum', category: 'Eye', percentage: 32 },
+  { id: '#box5', title: 'Lorem ipsum', category: 'Glasses', percentage: 3 },
+  {
+    id: '#box6',
+    title: 'Lorem ipsum',
+    category: 'Hair & Hats',
+    percentage: 2,
+  },
+  { id: '#box7', title: 'Lorem ipsum', category: 'Head', percentage: 92 },
+  {
+    id: '#box8',
+    title: 'Lorem ipsum',
+    category: 'Legs & Clothes',
+    percentage: 2,
+  },
+  { id: '#box9', title: 'Lorem ipsum', category: 'Mouth', percentage: 46 },
+  { id: '#box10', title: 'Lorem ipsum', category: 'Lorem', percentage: 5 },
+  { id: '#box11', title: 'Lorem ipsum', category: 'Lorem', percentage: 15 },
+  { id: '#box12', title: 'Lorem ipsum', category: 'Lorem', percentage: 25 },
+  { id: '#box13', title: 'Lorem ipsum', category: 'Lorem', percentage: 35 },
+];
