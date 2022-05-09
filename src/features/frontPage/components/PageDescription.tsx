5f28e507d7675e4badaa6742ee1be2c1087dac2e
RusinTomasz <tomaszrusin90@gmail.com>
2022-05-09 18:59:39 +0200

    feat: add rwd classes to PageDescription and PageLinkSection

--- src/features/frontPage/components/PageDescription.tsx
+++ src/features/frontPage/components/PageDescription.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export const PageDescription = () => {
   return (
-    <section className='container mt-[89px] text-center'>
+    <section className='container mt-[89px] text-center sm:mt-[50px]'>
       <h1>Lorem ipsum</h1>
-      <p className='mt-1.5 text-base leading-[20px] text-whiteText'>
+      <p className='mt-1.5 text-base leading-[20px] text-whiteText sm:text-sm'>
         Created by: <span className='text-red'>Lorem ipsum</span>
       </p>
-      <p className='mt-3 text-base leading-[22px]'>
+      <p className='mt-3 text-base leading-[22px] sm:text-sm'>
         Sed ut perspiciatis unde omnis iste natus error sit voluptatem.
       </p>
     </section>
--- src/features/frontPage/components/PageLinksSection.tsx
+++ src/features/frontPage/components/PageLinksSection.tsx
@@ -8,24 +8,27 @@ import TagIcon from '@/assets/svgs/tag-icon.svg';
 
 export const PageLinksSection = () => {
   return (
-    <section className='container mt-[70px] flex items-center justify-center'>
+    <section className='container mt-[50px] flex flex-wrap items-center justify-center'>
       <BaseLink
         label='Lorem ipsum'
         url='/'
         Icon={<StepsIcon />}
         variant='contained'
+        classes='mt-5'
       />
       <BaseLink
         label='Lorem ipsum'
         url='/'
         Icon={<FourCirclesIcon />}
         variant='contained'
+        classes='mt-5'
       />
       <BaseLink
         label='Lorem ipsum'
         url='/'
         Icon={<TagIcon className='mb-0.5 text-[17px]' />}
         variant='contained'
+        classes='mt-5'
       />
     </section>
   );
