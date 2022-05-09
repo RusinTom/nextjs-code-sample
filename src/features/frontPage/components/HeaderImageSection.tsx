619d8e2c6f1b2ce4bed48c5e03f60fae01c38e82
RusinTomasz <tomaszrusin90@gmail.com>
2022-05-09 19:02:45 +0200

    style: add rwd classes for HeaderImageSection

--- src/features/frontPage/components/HeaderImageSection.tsx
+++ src/features/frontPage/components/HeaderImageSection.tsx
@@ -3,18 +3,19 @@ import React from 'react';
 
 export const HeaderImageSection = () => {
   return (
-    <section className='relative m-auto h-[264px] w-[1400px] '>
+    <section className='relative m-auto w-[1440px] max-w-full px-5'>
       <div className='relative h-full overflow-hidden rounded'>
         <Image
           className='w-full object-contain'
           src='/images/jpg/header-image.jpg'
           alt=''
-          layout='fill'
+          height={264}
+          width={1400}
         />
       </div>
 
-      <div className='absolute -bottom-[63px] left-1/2 -translate-x-[63px]'>
-        <Image src='/images/png/eye.png' alt='' width={149} height={149} />
+      <div className='absolute -bottom-[63px] left-1/2 h-[149px] w-[149px] -translate-x-[63px] xl:-bottom-[58px] xl:h-[129px] xl:w-[129px] xl:-translate-x-[56px] lg:-bottom-[56px] lg:h-[110px] lg:w-[110px] lg:-translate-x-[48px] md:-bottom-[55px] md:h-[100px] md:w-[100px] md:-translate-x-[44px] sm:-bottom-[59px] sm:-bottom-[52px] sm:h-[80px] sm:w-[80px] sm:-translate-x-[36px] xsm:-bottom-[27px] xsm:h-[50px] xsm:w-[50px] xsm:-translate-x-[22px]'>
+        <Image src='/images/png/eye.png' alt='' layout='fill' />
       </div>
     </section>
   );
