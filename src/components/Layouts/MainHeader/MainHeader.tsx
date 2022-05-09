b3aff44a87987904516ad41fc57d4db2092a97a4
RusinTomasz <tomaszrusin90@gmail.com>
2022-05-09 19:03:26 +0200

    feat: add MobileMenu to MainHeader and adjust styles for SearchBar

--- src/components/Layouts/MainHeader/MainHeader.tsx
+++ src/components/Layouts/MainHeader/MainHeader.tsx
@@ -1,12 +1,18 @@
 import Link from 'next/link';
 import React from 'react';
 
+import { useMobileMenu } from '@/hooks/useMobileMenu';
+
 import { MainNavigation } from '@/components/MainNavigation/MainNavigation';
+import { MobileMenu } from '@/components/MobileMenu/MobileMenu';
+import { MobileMenuHamburger } from '@/components/MobileMenu/MobileMenuHamburger';
 import { SearchBar } from '@/components/SearchBar/SearchBar';
 
 import LogoWhite from '../../../assets/svgs/logo-white.svg';
 
 export const MainHeader = () => {
+  const { isOpen, setIsOpen } = useMobileMenu();
+
   return (
     <header>
       <div className='flex items-center justify-between bg-black px-10 pt-[26px] pb-[25px]'>
@@ -17,6 +23,8 @@ export const MainHeader = () => {
         </Link>
         <SearchBar />
         <MainNavigation />
+        <MobileMenuHamburger setIsOpen={setIsOpen} />
+        <MobileMenu setIsOpen={setIsOpen} isOpen={isOpen} />
       </div>
       <div className='bg- h-1.5' style={{ background: 'var(--top-bar)' }} />
     </header>
