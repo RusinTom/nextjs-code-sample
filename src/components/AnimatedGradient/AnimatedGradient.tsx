bcf3a94a736649879c26ba4a8941c320e3bdc45c
RusinTomasz <tomaszrusin90@gmail.com>
2022-05-10 23:01:46 +0200

    feat: add changeGradient color on mouseMove

--- src/components/AnimatedGradient/AnimatedGradient.tsx
+++ src/components/AnimatedGradient/AnimatedGradient.tsx
@@ -1,25 +1,34 @@
-import { motion, useViewportScroll } from 'framer-motion';
+import { motion } from 'framer-motion';
 import _ from 'lodash';
 import React, { useEffect, useState } from 'react';
 
 export const AnimatedGradient = () => {
-  const { scrollYProgress } = useViewportScroll();
-
-  const [value, setValue] = useState(0);
+  const [style, setStyle] = useState(
+    `linear-gradient( -164.26deg, rgba(46 , 45, 102, 0.6) -2.5%, rgba(58,39,63, 0.6) 47.55%, #181818 90.7% )`
+  );
 
   useEffect(() => {
-    setValue(scrollYProgress.get());
-    scrollYProgress.onChange(_.throttle((v) => setValue(v), 100));
-  }, [scrollYProgress]);
+    const changeGradientOnMouseMove = _.throttle((e: MouseEvent) => {
+      const documentHeight = document.documentElement.scrollHeight;
+      const mouseYProgress = e.pageY / documentHeight;
+      setStyle(
+        `linear-gradient( -164.26deg, rgba(46, 45, 102, 0.6) -2.5%, rgba(${
+          58 * (1 + mouseYProgress)
+        }, ${39 * (1 + mouseYProgress)}, ${
+          63 * (1 + mouseYProgress)
+        }, 0.6) 47.55%, #181818 90.7% )`
+      );
+    }, 50);
+    document.addEventListener('mousemove', changeGradientOnMouseMove);
+    return () => {
+      document.removeEventListener('mousemove', changeGradientOnMouseMove);
+    };
+  }, []);
 
   return (
     <motion.div
       style={{
-        background: `linear-gradient( -164.26deg, rgba(46, 45, 102, 0.6) -2.5%, rgba(${
-          58 * (1 + value)
-        }, ${39 * (1 + value)}, ${
-          63 * (1 + value)
-        }, 0.6) 47.55%, #181818 90.7% )`,
+        background: style,
       }}
       className='z-negative absolute top-0 left-0 h-full w-full'
     />
