e07badb8f8de4a92fa50bfac1b7597b9cc310822
RusinTomasz <tomaszrusin90@gmail.com>
2022-05-11 08:44:13 +0200

    refactor: refactor gradient animation

--- src/components/AnimatedGradient/AnimatedGradient.tsx
+++ src/components/AnimatedGradient/AnimatedGradient.tsx
@@ -1,23 +1,33 @@
 import { motion } from 'framer-motion';
 import _ from 'lodash';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export const AnimatedGradient = () => {
-  const [style, setStyle] = useState(
-    `linear-gradient( -164.26deg, rgba(46 , 45, 102, 0.6) -2.5%, rgba(58,39,63, 0.6) 47.55%, #181818 90.7% )`
-  );
+  const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
+    const getCssProperty = (
+      styleDeclaration: CSSStyleDeclaration,
+      property: string
+    ) => {
+      return +styleDeclaration.getPropertyValue(property) ?? 1;
+    };
+
     const changeGradientOnMouseMove = _.throttle((e: MouseEvent) => {
-      const documentHeight = document.documentElement.scrollHeight;
-      const mouseYProgress = e.pageY / documentHeight;
-      setStyle(
-        `linear-gradient( -164.26deg, rgba(46, 45, 102, 0.6) -2.5%, rgba(${
-          58 * (1 + mouseYProgress)
-        }, ${39 * (1 + mouseYProgress)}, ${
-          63 * (1 + mouseYProgress)
-        }, 0.6) 47.55%, #181818 90.7% )`
-      );
+      if (ref.current) {
+        const documentHeight = document.documentElement.scrollHeight;
+        const mouseYProgress = e.pageY / documentHeight;
+
+        const documentStyles = getComputedStyle(document.documentElement);
+
+        const r = getCssProperty(documentStyles, '--r') * (1 + mouseYProgress);
+        const g = getCssProperty(documentStyles, '--g') * (1 + mouseYProgress);
+        const b = getCssProperty(documentStyles, '--b') * (1 + mouseYProgress);
+
+        ref.current.style.setProperty('--r', `${r}`);
+        ref.current.style.setProperty('--g', `${g}`);
+        ref.current.style.setProperty('--b', `${b}`);
+      }
     }, 50);
     document.addEventListener('mousemove', changeGradientOnMouseMove);
     return () => {
@@ -27,10 +37,8 @@ export const AnimatedGradient = () => {
 
   return (
     <motion.div
-      style={{
-        background: style,
-      }}
-      className='z-negative absolute top-0 left-0 h-full w-full'
+      ref={ref}
+      className='z-negative purple-gradient absolute top-0 left-0 h-full w-full'
     />
   );
 };
